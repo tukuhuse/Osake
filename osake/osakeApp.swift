4be08f9dfa944f00be60ba3649a6a9f1b9cbc5c0
tukuhuse <tukuhuse@icloud.com>
2021-06-27 23:17:26 +0900

    Initial Commit

---
+++ osake/osakeApp.swift
@@ -0,0 +1,20 @@
+//
+//  osakeApp.swift
+//  osake
+//
+//  Created by 高橋優人 on 2021/06/27.
+//
+
+import SwiftUI
+
+@main
+struct osakeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
