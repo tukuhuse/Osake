4fba5f020ff35ada80b4b31114248815a8baee7f
tukuhuse <tukuhuse@icloud.com>
2021-06-29 23:30:32 +0900

    previewの初期値追加、Categoryの追加ボタン作成中

--- osake/View/CategorySelect.swift
+++ osake/View/CategorySelect.swift
@@ -11,28 +11,22 @@ import CoreData
 struct CategorySelect: View {
     
     @Environment(\.managedObjectContext) private var viewContext
-    
-    /*
-    @State var categorylist: FetchRequest<Category>
-    init(kind: Int16) {
-        categorylist = FetchRequest<Category>(entity: Category.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)], predicate: NSPredicate(format: "kind == %d", kind), animation: .default)
-    }
-    */
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)], animation: .default) var CategoryList: FetchedResults<Category>
     
     let kind: Category.kind
     
-    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)], animation: .default) var CategoryList: FetchedResults<Category>
-    
-    //var kindlist: [Category.kind] = [.liquor,.spirit]
-    
     var body: some View {
-        Form {
-            Section(header: Text("\(kind.toString())")) {
-                List {
-                    ForEach(CategoryList, id:\.self) { category in
-                        if self.kind.rawValue == category.kind {
-                            Text(category.name!)
-                        }
+        Section(header: HStack{
+                    Text("\(kind.toString())")
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("追加")
+            })
+        }) {
+            List {
+                ForEach(CategoryList, id:\.self) { category in
+                    if self.kind.rawValue == category.kind {
+                        Text(category.name!)
                     }
                 }
             }
@@ -51,9 +45,20 @@ struct CategorySelect_Previews: PreviewProvider {
         let request = NSBatchDeleteRequest(fetchRequest: NSFetchRequest(entityName: "Category"))
         try! container.persistentStoreCoordinator.execute(request, with: viewContext)
         
-        Category.create(in: viewContext, name: "sample1", kind: .liquor)
-        Category.create(in: viewContext, name: "sample2", kind: .spirit)
+        //醸造酒の初期データ
+        Category.create(in: viewContext, name: "日本酒", kind: .liquor)
+        Category.create(in: viewContext, name: "ビール", kind: .liquor)
+        Category.create(in: viewContext, name: "ワイン", kind: .liquor)
         
-        return CategorySelect(kind: .liquor).environment(\.managedObjectContext, viewContext)
+        //蒸留酒の初期データ
+        Category.create(in: viewContext, name: "焼酎", kind: .spirit)
+        Category.create(in: viewContext, name: "ウイスキー", kind: .spirit)
+        Category.create(in: viewContext, name: "ブランデー", kind: .spirit)
+        Category.create(in: viewContext, name: "ウォッカ", kind: .spirit)
+        
+        return Form{
+            CategorySelect(kind: .liquor).environment(\.managedObjectContext, viewContext)
+            CategorySelect(kind: .spirit).environment(\.managedObjectContext, viewContext)
+        }
     }
 }
